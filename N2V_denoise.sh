29548b74356efbaeb96afbfa6c51b2b0b59d7e1e
kiki394 <dario_bernasconi@icloud.com>
2025-04-15 18:56:31 +0200

    Update and rename 00_denoise_n2v.sh to N2V_denoise.sh

---
+++ N2V_denoise.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --time=1-0:00:00
+#SBATCH --mem 128GB
+#SBATCH --ntasks=8
+#SBATCH --gres=gpu:1
+
+IMG_DIR=
+MODEL_BASE_DIR=
+CHANNEL_LIST=(red green)
+PROJECT=True
+
+source $HOME/miniforge3/bin/activate n2v
+
+FILE_LIST=(`ls ${IMG_DIR}`)
+
+for IMG_NAME in ${FILE_LIST[@]}
+do
+  echo "Processing" $IMG_NAME
+  python ./Scripts/N2Vdenoise.py -d $IMG_DIR -i $IMG_NAME -m $MODEL_BASE_DIR -c ${CHANNEL_LIST[@]} -p $PROJECT
+done

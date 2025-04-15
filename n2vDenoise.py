de788953b103a060eb9775dcafefc914437a77b3
kiki394 <dario_bernasconi@icloud.com>
2025-04-15 18:59:16 +0200

    Update n2vDenoise.py

--- n2vDenoise.py
+++ n2vDenoise.py
@@ -1,145 +1,104 @@
-#!/usr/bin/env python3
-
-from n2v.models import N2VConfig, N2V
-import numpy as np
-#from nis2pyr.convertor import convert_nd2_to_pyramidal_ome_tiff
-from csbdeep.utils import plot_history
-from csbdeep.io import save_tiff_imagej_compatible
-from n2v.utils.n2v_utils import manipulate_val_data
-from n2v.internals.N2V_DataGenerator import N2V_DataGenerator
-from matplotlib import pyplot as plt
-import urllib
-#import nd2
-from nd2reader import ND2Reader
-import os
-import re
-import zipfile
 import argparse
-from tifffile import imread, imwrite
+import os
+from os.path import join, isdir, basename
+from os import makedirs
+from bioio import BioImage
+from matplotlib import pyplot as plt
+import numpy as np
+from n2v.models import N2V
+from image_utils import *
+
 os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'
 
-import ssl
+def split_nd(img_dir, img_name, channel_list, model_base_dir, project=True):
+	denoised_dir = join(img_dir, basename(model_base_dir), 'denoised')
+	if not isdir(denoised_dir):
+		makedirs(denoised_dir, exist_ok=True) # Make a directory for the denoised images if it does not exist yet
+	img = join(img_dir, img_name)
+	dims = ''.join([i for i in ['T', 'C'] if BioImage(img).dims[i][0] > 1])
+	tcz_path = split_stack(img, dims=dims, keep_img=True) # Split images by time point and/or channel
+	temp_path = join(denoised_dir, f'{img_name[:img_name.rfind(".")]}_temp.zarr')
+	root = zarr.group(store=temp_path)
+	for i in range(len(channel_list)): # Loop over the channels defined in the .sh file
+		to_denoise = []
+		groups, arrays = unpack_zarr(tcz_path)
+		for array in arrays:
+			if 'c' + str(i).zfill(3) in array:
+				to_denoise.append(array)
+		for array in to_denoise:
+			data = zarr.open_array(array)
+			denoised_data = n2v_denoise(data=data,
+			img_dir=img_dir,
+			img_name=img_name,
+			model_base_dir=model_base_dir,
+			channel=channel_list[i],
+			plotInputPredict=True
+			)
+			root.array(name=f'{basename(array)}_n2v', data=denoised_data)
+	stack_paths = stack_images(imgs=temp_path, dims=dims, keep_imgs=False)	
+	for path in stack_paths:
+		print(f'project is set to {project}')
+		print(type(project))
+		tif_path = zarr_to_tif(zarr_file=path, keep_img=False)
+		print(tif_path)
+		if project == True:
+			print('True')
+			print(tif_path)
+			project_image(tif_path)
+		else:
+			print('NO')
+	shutil.rmtree(tcz_path)
 
-def n2vDenoise(model_dir, model_name, image_dir, image_name, channel_name, plotInputPredict=False):
-	'''
-	Take in name and location of a n2v trained model and a single channel image
-	and denoise
-	'''
-	print("\n\nDenoising image...\n\n")
-	# A previously trained model is loaded by creating a new N2V-object without providing a 'config'.
-	model = N2V(config=None, name=channel_name, basedir=model_dir)
-	# We load the data we want to process.
-	split_image_dir=os.path.join(image_dir, 'n2v/TMP/' + channel_name)
-	split_image_name=re.sub("\.nd2", "_" + channel_name + ".tif", image_name)
-	img = imread(os.path.join(split_image_dir, split_image_name))
-
-	# Here we process the data.
-	# The 'n_tiles' parameter can be used if images are too big for the GPU memory.
-	# If we do not provide the 'n_tiles' parameter the system will automatically try to find an appropriate tiling.
-	pred = model.predict(img, axes='ZYX', n_tiles=(1,4,4))
-	
-	# output flattened pngs of input and predicted images
-	if(plotInputPredict):
-		inputpredict_path=os.path.join(image_dir,'n2v/inputPredict/')
-		if not os.path.isdir(inputpredict_path):
-			os.makedirs(inputpredict_path, exist_ok=True)
-		
-		# Let's look at the results.
-		plt.figure(figsize=(30,30))
-		
-		# We show the noisy input...
-		plt.subplot(1,2,1)
-		plt.imshow(np.max(img[...],axis=0),
-				   cmap='gray',
-				   vmin=np.percentile(img,0.1),
-				   vmax=np.percentile(img,99.9)
-				  )
+def n2v_denoise(data, img_dir, img_name, model_base_dir, channel, plotInputPredict=False):
+	model = N2V(config=None, name=channel, basedir=model_base_dir) # Load the trained model
+	# Pass the input image (a numpy array) to the denoising function
+	# T and C of the input have a size of one already and are specified only to reduce the shape of the array from 5D to 3D
+	# n_tiles is to split up the image in case it is too big for the RAM and will be set automatically if not specified
+	denoised_data = model.predict(data[0, 0], axes='ZYX', n_tiles=(1, 4, 4))
+	if(plotInputPredict): # Plot a flattened image of both the input and the predited (i.e., denoised) image
+		inputpredict_dir = join(img_dir, basename(model_base_dir), 'inputPredict')
+		if not isdir(inputpredict_dir):
+			makedirs(inputpredict_dir, exist_ok=True)
+		plt.figure(figsize=(30, 30))
+		plt.subplot(1, 2, 1) # Plot the input image...
+		plt.imshow(
+			np.max(data[0, 0], axis=0), 
+			cmap = 'gray',
+			vmin = np.percentile(data, 0.1),
+			vmax = np.percentile(data, 99.9))
 		plt.title('Input');
-		# and the result.
-		plt.subplot(1,2,2)
-		plt.imshow(np.max(pred[...],axis=0), 
-				   cmap='gray',
-				   vmin=np.percentile(pred,0.1),
-				   vmax=np.percentile(pred,99.9)
-				  )
-		plt.title('Prediction');
-		plt.savefig(os.path.join(inputpredict_path, "InputPredict_" +re.sub("\.nd2", "_" + channel_name + ".tif", image_name)))
-	
-	# output denoised images
-	denoised_path=os.path.join(image_dir,'n2v/denoised/')
-	if not os.path.isdir(denoised_path):
-			os.makedirs(denoised_path, exist_ok=True)
-	imwrite(os.path.join(denoised_path, re.sub("\.nd2", "_" + channel_name + "_n2v.tif", image_name)), pred)
-	return None
+		plt.subplot(1, 2, 2) # ...and the denoised one
+		plt.imshow(
+			np.max(denoised_data, axis=0),
+			cmap = 'gray',
+			vmin = np.percentile(denoised_data, 0.1),
+			vmax = np.percentile(denoised_data, 99.9))
+		plt.title('Prediction')
+		plt.savefig(join(inputpredict_dir, "InputPredict_" + img_name[0 : img_name.rfind(".")] + '_' + channel + '.tif'))
+	denoised_data = np.expand_dims(np.expand_dims(denoised_data, axis=0), axis=0) # This is to makes sure the image stays 5D
+	return denoised_data
 
-def splitChannels(image_dir, image_name, channel_list):
-	'''
-	Split nd2 image into separate color channels and save each to a temporary 
-	directory named after the channel to be used as input for denoising
-	'''
-	print("\n\nSplitting images and converting them to tiff\n\n")
-	img = ND2Reader(os.path.join(image_dir, image_name))
-	print("Image shape is: "+str(img.shape))
-	if (len(img.metadata['channels'])>1):
-	# if(len(img.shape)>3):
-		print("splitting channels")
-		img.bundle_axes = 'zyx'
-		img.iter_axes = 'c'
-		for i in range(len(channel_list)): 
-			tempChannel_path=os.path.join(image_dir, 'n2v/TMP/' + channel_list[i])
-			if not os.path.isdir(tempChannel_path):
-				os.makedirs(tempChannel_path, exist_ok=True)
-			imwrite(os.path.join(tempChannel_path, re.sub("\.nd2" ,  "_" + channel_list[i] + ".tif", image_name)), img[i])
-	else:
-		print("saving directly as tiff")
-		tempChannel_path=os.path.join(image_dir, 'n2v/TMP/' + channel_list[0])
-		if not os.path.isdir(tempChannel_path):
-			os.makedirs(tempChannel_path, exist_ok=True)
-		imwrite(os.path.join(tempChannel_path, re.sub("\.nd2" ,  "_" + channel_list[0] + ".tif", image_name)), img)
-	return None
-
-
-
-def get_args():
-	'''get command line arguments'''
+def get_args(): # Parse arguments from the shell script
 	parser = argparse.ArgumentParser()
-	parser.add_argument("-d","--image_dir", help="Path to directory containing nd2 images", type=str, required=True)
-	parser.add_argument("-i","--image_name", help="Name of nd2 image to be processed", type=str, required=True)
-	parser.add_argument("-m","--model_dir", help="Path to directory containing the models", type=str, required=True)
-	parser.add_argument("-n","--model_base_name", help="Base name of the models needed for denoising",default='n2v_3D_CREST_')
-	parser.add_argument("-c","--channel_list", action="extend", nargs="+", type=str, help="Ordered list of channel names",required=True,default=[])
+	parser.add_argument("-d", "--img_dir", help="Path to directory containing the images to denoise", type=str, required=True)
+	parser.add_argument("-i", "--img_name", help="Name of image to denoised", type=str, required=True)
+	parser.add_argument("-c", "--channel_list", action="extend", nargs="+", type=str, help="Ordered list of channel names", required=True, default=[])
+	parser.add_argument("-m", "--model_base_dir", help="Path to directory containing the models", type=str, required=True)
+	parser.add_argument("-p", "--project", help="Whether to save a projection of each denoised image", type=bool, required=True)
 	return parser.parse_args()
 
-
 def main():
-	'''main'''
-	args = get_args()
-	image_dir=args.image_dir
-	image_name=os.path.basename(args.image_name)
-	model_dir=args.model_dir
-	model_base_name=args.model_base_name
-	channel_list=args.channel_list
-	print("channels are:")
-	print(*channel_list)
-	# split channels and convert to tiff 
-	splitChannels(image_dir, image_name, channel_list)
-	# cycle through and denoise
-	for channel_name in channel_list:
-		model_name = model_base_name + '_' + channel_name
-		n2vDenoise(model_dir, model_name, image_dir, image_name, channel_name, plotInputPredict=True)
-		split_image_dir=os.path.join(image_dir, 'n2v/TMP/' + channel_name)
-		split_image_name=re.sub("\.nd2", "_" + channel_name + ".tif", image_name)
-		os.remove(os.path.join(split_image_dir,split_image_name))
-	return None
+	img_dir = get_args().img_dir
+	img_name = get_args().img_name
+	channel_list = get_args().channel_list
+	model_base_dir = get_args().model_base_dir
+	project = get_args().project
+	split_nd(img_dir=img_dir,
+	img_name=img_name,
+	channel_list=channel_list,
+	model_base_dir=model_base_dir,
+	project=project
+	) # Split stacks by timepoints and/or channels
 
 if __name__=='__main__':
 	main()
-
-# move to the folder with our data
-# channel_color='red'
-# os.chdir(workDir)
-# model_name = 'n2v_3D_CREST_green'
-# model_dir = '/mnt/external.data/MeisterLab/jsemple/'+'/n2v_denoise/training/models/'
-# image_name='C1-SAMS-3_L1s_0014_green.tif'
-# channel_list=["red","green"]
-
